1ceec6d49062e7172b4a40bf57654de8b8b9fa7a
Hai Lu <luhonghai@gmail.com>
2018-07-10 11:20:48 +0700

    Use ganache CLI to run test

--- truffle.js
+++ truffle.js
@@ -1,12 +1,16 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
-
+const ganache = require('ganache-cli');
 module.exports = {
-  networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 8545,
-      network_id: '*' // Match any network id
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: '*' // Match any network id
+        },
+        ganache_cli: {
+            provider: ganache.provider(),
+            network_id: '*'
+        }
     }
-  }
-}
+};
